8a3a46cc2db6a2d6c80e9bcea3cb3ca66ba9b13c
Camilo García <candres-garcia@javeriana.edu.co>
2022-10-16 21:22:22 -0500

    Lab upload

---
+++ WebApplication1/Models/Entities/Profesion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models.Entities
+{
+    public partial class Profesion
+    {
+        public Profesion()
+        {
+            Estudios = new HashSet<Estudio>();
+        }
+
+        public int Id { get; set; }
+        public string Nom { get; set; } = null!;
+        public string Des { get; set; } = null!;
+
+        public virtual ICollection<Estudio> Estudios { get; set; }
+    }
+}
